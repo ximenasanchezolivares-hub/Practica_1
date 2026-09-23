f937ddff6ddde455230e80b209504ad1e7af9a21
ximenasanchezolivares-hub <ximena.sanchez.olivares@estudiantxs.uacm.edu.mx>
2026-09-23 15:08:27 -0600

    Listado de los equipos

--- practica-1.c
+++ practica-1.c
@@ -11,6 +11,9 @@ int validarTipoEvento(int tipo) {
     }
     return 0; // Inválido
 }
+void calcularSumaReferencia(int *resultado, int incorrecto, int inexistente) {
+    *resultado = incorrecto + inexistente;
+}
 
 int main() {
     int i;
@@ -21,7 +24,9 @@ int main() {
     
     int equipos[TOTAL_EVENTOS] = {0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3};
     int eventos_tipos[TOTAL_EVENTOS]   = {0, 1, 1, 0, 1, 2, 1, 1, 0, 1, 0, 2, 1, 1, 2, 0, 1, 2, 1, 0};
+    const char *nombres_equipos[NUM_EQUIPOS] = {"PC-01", "PC-02", "PC-03", "PC-04"};
     int matriz_equipos[NUM_EQUIPOS][TIPOS_EVENTOS] = {0};  
+    
 
 for (i = 0; i < TOTAL_EVENTOS; i++) {
         int eq = equipos[i];   
@@ -30,7 +35,13 @@ for (i = 0; i < TOTAL_EVENTOS; i++) {
         if (validarTipoEvento(tipo) == 1) {
             matriz_equipos[eq][tipo]++;
         }
+
     }
+    printf("--- LOG DE EVENTOS REGISTRADOS ---\n");
+    for (i = 0; i < TOTAL_EVENTOS; i++) {
+        printf("Evento %d -> Equipo: %s, Tipo de Evento: %d\n", i + 1, nombres_equipos[equipos[i]], eventos_tipos[i]);
+    }
+    printf("\n");
 
     for(i=0;i<NUM_EQUIPOS;i++){
         int *apt_fila=*(matriz_equipos+i);
@@ -38,9 +49,7 @@ for (i = 0; i < TOTAL_EVENTOS; i++) {
         ctr_incorrecto=*(apt_fila+1);
         usr_inexistente=*(apt_fila+2);
 
-         suma_intentos=ctr_incorrecto+usr_inexistente;
-        
-       // printf("%d\n",suma_intentos);
+        calcularSumaReferencia(&suma_intentos, ctr_incorrecto, usr_inexistente);
 
         if(suma_intentos>x){
             printf("%d %d %d SOSPECHOSO \n ",ctr_incorrecto,usr_inexistente,suma_intentos);
